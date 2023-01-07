d5ef2d2d1f37e171e68a91c2ac9d08312aeb533d
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-01-07 01:12:12 -0800

    changed format

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,13 @@ game 'rdr3'
 lua54 'yes'
 
 
-client_scripts { 'client/client.lua' }
+client_scripts {
+    'client/client.lua'
+}
 
-server_scripts { 'server/server.lua' }
+server_scripts {
+    'server/server.lua'
+}
 
 shared_scripts {
     'config.lua',
