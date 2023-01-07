c944f3b4980e62608dff3c787e6b18a5e6cf0035
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-01-07 01:10:57 -0800

    housekeeping

--- server/server.lua
+++ server/server.lua
@@ -6,28 +6,28 @@ end)
 -- Buy New Boats
 RegisterServerEvent('oss_portals:BuyPassage')
 AddEventHandler('oss_portals:BuyPassage', function(outletData, portId)
-    local _source = source
-    local Character = VORPcore.getUser(_source).getUsedCharacter
-    local money = Character.money
-    local gold = Character.gold
+    local src = source
+    local Character = VORPcore.getUser(src).getUsedCharacter
     local location = outletData.location
     local currencyType = outletData.currencyType
     local buyPrice = outletData.buyPrice
 
     if currencyType == "cash" then
+        local money = Character.money
         if money >= buyPrice then
             Character.removeCurrency(0, buyPrice)
-            TriggerClientEvent('oss_portals:SendPlayer', _source, location, portId)
+            TriggerClientEvent('oss_portals:SendPlayer', src, location, portId)
         else
-            VORPcore.NotifyRightTip(_source, _U("shortCash"), 4000)
+            VORPcore.NotifyRightTip(src, _U("shortCash"), 4000)
         end
 
     elseif currencyType == "gold" then
+        local gold = Character.gold
         if gold >= buyPrice then
             Character.removeCurrency(1, buyPrice)
-            TriggerClientEvent('oss_portals:SendPlayer', _source, location, portId)
+            TriggerClientEvent('oss_portals:SendPlayer', src, location, portId)
         else
-            VORPcore.NotifyRightTip(_source, _U("shortGold"), 4000)
+            VORPcore.NotifyRightTip(src, _U("shortGold"), 4000)
         end
     end
 end)
@@ -35,12 +35,12 @@ end)
 -- Check Player Job and Job Grade
 RegisterServerEvent('oss_portals:getPlayerJob')
 AddEventHandler('oss_portals:getPlayerJob', function()
-    local _source = source
-    if _source then
-        local Character = VORPcore.getUser(_source).getUsedCharacter
+    local src = source
+    if src then
+        local Character = VORPcore.getUser(src).getUsedCharacter
         local CharacterJob = Character.job
         local CharacterGrade = Character.jobGrade
 
-        TriggerClientEvent('oss_portals:sendPlayerJob', _source, CharacterJob, CharacterGrade)
+        TriggerClientEvent('oss_portals:sendPlayerJob', src, CharacterJob, CharacterGrade)
     end
 end)
