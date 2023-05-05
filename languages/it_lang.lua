b48000d64372f4c81eeaeb436acce9639f2b0ee5
Fenixs <33010033+FenixsGame@users.noreply.github.com>
2023-05-06 00:20:54 +0200

    Add files via upload
    Translated into Italian

---
+++ languages/it_lang.lua
@@ -0,0 +1,15 @@
+---------------------------------- ITALIAN -------------------------------------------
+Locales["it_lang"] = {
+
+    portPrompt = "Menu",
+    subMenu = "Destinazioni disponibili",
+    closed = " ~o~: ~e~CHIUSO",
+    to = ":00~q~ a ~o~",
+    hundred = ":00",
+    hours = " è aperto dalle ~o~",
+    shortGold = "Non hai abbastanza oro",
+    shortCash = "Non hai abbastanza dollari",
+    needJob = "Per viaggiare hai bisogno del lavoro: ~o~",
+    price = "Costo: ",
+    traveling = "Viaggiando verso "
+}
