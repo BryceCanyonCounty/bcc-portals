817e93a0a3ae64dda6daf67e68a443465445edc6
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-06 19:01:32 -0700

    update server.lua
    Simplified distance check for computing cost and time for travel.

--- server/server.lua
+++ server/server.lua
@@ -4,11 +4,9 @@ TriggerEvent('getCore', function(core)
 end)
 
 -- Get Travel Time and Price Data
-RegisterNetEvent('bcc-portals:GetData', function(location, pcoords,shopId)
+RegisterNetEvent('bcc-portals:GetData', function(location, pCoords,shopId)
     local _source = source
-    local shopConfig = Config.shops[location]
-    local dcoords = vector3(shopConfig.npc.x, shopConfig.npc.y, shopConfig.npc.z)
-    local distance = #(pcoords - dcoords)
+    local distance = #(pCoords - Config.shops[location].npc)
     local cashPrice = 0
     local goldPrice = 0
     if Config.price > 0 then
@@ -25,18 +23,17 @@ end)
 RegisterNetEvent('bcc-portals:BuyPassage', function(location, price, time, isCash)
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
-    local buyPrice = price
 
     if isCash then
-        if Character.money >= buyPrice then
-            Character.removeCurrency(0, buyPrice)
+        if Character.money >= price then
+            Character.removeCurrency(0, price)
             TriggerClientEvent('bcc-portals:SendPlayer', _source, location, time)
         else
             VORPcore.NotifyRightTip(_source, _U('shortCash'), 4000)
         end
     else
-        if Character.gold >= buyPrice then
-            Character.removeCurrency(1, buyPrice)
+        if Character.gold >= price then
+            Character.removeCurrency(1, price)
             TriggerClientEvent('bcc-portals:SendPlayer', _source, location, time)
         else
             VORPcore.NotifyRightTip(_source, _U('shortGold'), 4000)
