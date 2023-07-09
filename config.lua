368811a5bb5078a0b4e30c4018ed98ed3503f371
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-08 19:06:48 -0700

    move currency option to each shop
    - Can set individual shops to use: cash-only, gold-only, cash and gold, free
    - Housekeeping

--- config.lua
+++ config.lua
@@ -14,8 +14,7 @@ Config.time = 3.0 -- Default: 3.0 / Travel Time Multiplier
 -----------------------------------------------------
 
 -- Price is Computed Based on Distance
-Config.currency = 3 -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = no-currency (set Config.price to 0)
-Config.price = 0.012 -- Default: 0.012 / Price Multiplier / 0 = No Charge
+Config.price = 0.012 -- Default: 0.012 / Price Multiplier
 -----------------------------------------------------
 
 -- Allow Blips on Map when Shop is Closed
@@ -33,27 +32,28 @@ Config.shops = {
         blipOpen = 'WHITE', -- Shop Open - Default: White - Blip Colors Shown Below
         blipClosed = 'RED', -- Shop Closed - Deafault: Red - Blip Colors Shown Below
         blipJob = 'YELLOW_ORANGE', -- Shop Job Locked - Default: Yellow - Blip Colors Shown Below
+        npcOn = true, -- Turns NPCs On / Off
+        npcModel = 's_m_m_sdticketseller_01', -- Sets Model for NPCs
         npc = vector3(2941.59, 1273.58, 43.64), -- NPC and Shop Blip Positions
         npcHeading =  237.13, -- NPC Heading
         player = vector4(2942.39, 1272.97, 44.64, 52.18), -- Player Teleport Position
         nDistance = 100.0, -- Distance from Shop for NPC to Spawn
         sDistance = 2.0, -- Distance from NPC to Get Menu Prompt
-        npcOn = true, -- Turns NPCs On / Off
-        npcModel = 's_m_m_sdticketseller_01', -- Sets Model for NPCs
         allowedJobs = {}, -- Empty, Everyone Can Use / Insert Job to limit access - ex. 'police'
         jobGrade = 0, -- Enter Minimum Rank / Job Grade to Access Shop
         shopHours = false, -- If You Want the Shops to Use Open and Closed Hours
         shopOpen = 7, -- Shop Open Time / 24 Hour Clock
         shopClose = 21, -- Shop Close Time / 24 Hour Clock
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = { -- label is the name used in the body of the menu / DO NOT CHANGE 'location'
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     armadillo = {
@@ -65,27 +65,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(-3724.68, -2602.43, -13.91),
         npcHeading =  246.94,
         player = vector4(-3724.04, -2603.28, -12.94, 16.65),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     blackwater = {
@@ -97,27 +98,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(-732.87, -1232.39, 43.73),
         npcHeading =  133.34,
         player = vector4(-733.99, -1233.41, 44.73, 313.2),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     rhodes = {
@@ -129,27 +131,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(1233.52, -1287.75, 75.97),
         npcHeading =  284.16,
         player = vector4(1234.95, -1287.43, 76.97, 103.86),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     strawberry = {
@@ -161,27 +164,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(-1836.82, -422.29, 159.86),
         npcHeading =  244.28,
         player = vector4(-1835.75, -422.89, 160.83, 58.27),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     stdenis = {
@@ -193,27 +197,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(2740.99, -1391.48, 45.21),
         npcHeading =  17.09,
         player = vector4(2740.67, -1390.49, 46.23, 201.54),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 3, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     tumbleweed = {
@@ -225,27 +230,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(-5495.56, -2958.57, -1.69),
         npcHeading =  314.82,
         player = vector4(-5494.4, -2957.35, -0.71, 149.64),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 1, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Valentine',   location = 'valentine',  },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Valentine',   location = 'valentine'  },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     valentine = {
@@ -257,27 +263,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(-184.38, 642.8, 112.59),
         npcHeading =  50.3,
         player = vector4(-185.79, 643.63, 113.44, 239.31),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 4, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Van Horn',    location = 'vanhorn',    },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Van Horn',    location = 'vanhorn'    }
         }
     },
     vanhorn = {
@@ -289,27 +296,28 @@ Config.shops = {
         blipOpen = 'WHITE',
         blipClosed = 'RED',
         blipJob = 'YELLOW_ORANGE',
+        npcOn = true,
+        npcModel = 's_m_m_sdticketseller_01',
         npc = vector3(2893.08, 623.51, 56.72),
         npcHeading =  64.27,
         player = vector4(2891.88, 623.66, 57.77, 254.74),
         nDistance = 100.0,
         sDistance = 2.0,
-        npcOn = true,
-        npcModel = 's_m_m_sdticketseller_01',
         allowedJobs = {},
         jobGrade = 0,
         shopHours = false,
         shopOpen = 7,
         shopClose = 21,
+        currency = 2, -- 1 = cash-only / 2 = gold-only / 3 = cash and gold / 4 = free
         outlets = {
-            { label = 'Annesburg',   location = 'annesburg',  },
-            { label = 'Armadillo',   location = 'armadillo',  },
-            { label = 'Blackwater',  location = 'blackwater', },
-            { label = 'Rhodes',      location = 'rhodes',     },
-            { label = 'Strawberry',  location = 'strawberry', },
-            { label = 'Saint Denis', location = 'stdenis',    },
-            { label = 'Tumbleweed',  location = 'tumbleweed', },
-            { label = 'Valentine',   location = 'valentine',  },
+            { label = 'Annesburg',   location = 'annesburg'  },
+            { label = 'Armadillo',   location = 'armadillo'  },
+            { label = 'Blackwater',  location = 'blackwater' },
+            { label = 'Rhodes',      location = 'rhodes'     },
+            { label = 'Strawberry',  location = 'strawberry' },
+            { label = 'Saint Denis', location = 'stdenis'    },
+            { label = 'Tumbleweed',  location = 'tumbleweed' },
+            { label = 'Valentine',   location = 'valentine'  }
         }
     }
 }
