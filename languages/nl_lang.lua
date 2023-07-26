b25a253f35eb6744382f7651328669f3772217db
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-26 15:23:50 -0700

    added dutch translation by eva

---
+++ languages/nl_lang.lua
@@ -0,0 +1,19 @@
+---------------------------------- DUTCH -------------------------------------------
+Locales["nl_lang"] = {
+
+    portPrompt = "Menu",
+    subMenu = "Beschikbare Portalen",
+    closed = " ~o~: ~e~SLUITEN",
+    to = ":00~q~ to ~o~",
+    hundred = ":00",
+    hours = " is geopend vanaf ~o~",
+    shortGold = "Je hebt niet voldoende goud!",
+    shortCash = "Je hebt niet voldoende geld!",
+    needJob = "Je hebt niet de juiste baan!",
+    traveling = "Reizen naar ",
+    destination = "Locatie: ",
+    choose = "Choose Your Locatie",
+    cash = "Geld",
+    gold = "Goud",
+    go = "Gaan"
+}
