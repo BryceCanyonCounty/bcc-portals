1a8337a63cf9ca38f9191bbc17b272fade223b74
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-07 01:56:38 -0700

    Update main.lua
    - set functions to local

--- client/main.lua
+++ client/main.lua
@@ -3,6 +3,81 @@ VORPcore = exports.vorp_core:GetCore()
 local PromptGroup = GetRandomIntInRange(0, 0xffffff)
 local HasJob = false
 
+local function StartPrompt()
+    MenuPrompt = PromptRegisterBegin()
+    PromptSetControlAction(MenuPrompt, Config.key)
+    PromptSetText(MenuPrompt, CreateVarString(10, 'LITERAL_STRING', _U('portPrompt')))
+    PromptSetVisible(MenuPrompt, true)
+    PromptSetStandardMode(MenuPrompt, true)
+    PromptSetGroup(MenuPrompt, PromptGroup)
+    PromptRegisterEnd(MenuPrompt)
+end
+
+local function ManageBlip(shop, closed)
+    local shopCfg = Locations[shop]
+
+    if closed and not shopCfg.blip.showClosed then
+        if Locations[shop].Blip then
+            RemoveBlip(Locations[shop].Blip)
+            Locations[shop].Blip = nil
+        end
+        return
+    end
+
+    if not Locations[shop].Blip then
+        shopCfg.Blip = Citizen.InvokeNative(0x554d9d53f696d002, 1664425300, shopCfg.npc.coords) -- BlipAddForCoords
+        SetBlipSprite(shopCfg.Blip, shopCfg.blip.sprite, true)
+        Citizen.InvokeNative(0x9CB1A1623062F402, shopCfg.Blip, shopCfg.blip.name) -- SetBlipNameFromPlayerString
+    end
+
+    local color = shopCfg.blip.color.open
+    if shopCfg.shop.jobsEnabled then color = shopCfg.blip.color.job end
+    if closed then color = shopCfg.blip.color.closed end
+    Citizen.InvokeNative(0x662D364ABF16DE2F, Locations[shop].Blip, joaat(Config.BlipColors[color])) -- BlipAddModifier
+end
+
+local function LoadModel(model, modelName)
+    if not IsModelValid(model) then
+        return print('Invalid model:', modelName)
+    end
+    RequestModel(model, false)
+    while not HasModelLoaded(model) do
+        Wait(10)
+    end
+end
+
+local function AddNPC(shop)
+    local shopCfg = Locations[shop]
+    if not shopCfg.NPC then
+        local modelName = shopCfg.npc.model
+        local model = joaat(modelName)
+        LoadModel(model, modelName)
+        shopCfg.NPC = CreatePed(model, shopCfg.npc.coords, shopCfg.npc.heading, false, false, false, false, false)
+        Citizen.InvokeNative(0x283978A15512B2FE, shopCfg.NPC, true) -- SetRandomOutfitVariation
+        SetEntityCanBeDamaged(shopCfg.NPC, false)
+        SetEntityInvincible(shopCfg.NPC, true)
+        Wait(500)
+        FreezeEntityPosition(shopCfg.NPC, true)
+        SetBlockingOfNonTemporaryEvents(shopCfg.NPC, true)
+    end
+end
+
+local function RemoveNPC(shop)
+    local shopCfg = Locations[shop]
+    if shopCfg.NPC then
+        DeleteEntity(shopCfg.NPC)
+        shopCfg.NPC = nil
+    end
+end
+
+local function CheckPlayerJob(shop)
+    HasJob = false
+    local result = VORPcore.Callback.TriggerAwait('bcc-portals:CheckJob', shop)
+    if result then
+        HasJob = true
+    end
+end
+
 CreateThread(function()
     StartPrompt()
     while true do
@@ -71,81 +146,6 @@ function SendPlayer(location, time)
     DoScreenFadeIn(1000)
     Wait(1000)
     SetCinematicModeActive(false)
-end
-
-function CheckPlayerJob(shop)
-    HasJob = false
-    local result = VORPcore.Callback.TriggerAwait('bcc-portals:CheckJob', shop)
-    if result then
-        HasJob = true
-    end
-end
-
-function StartPrompt()
-    MenuPrompt = PromptRegisterBegin()
-    PromptSetControlAction(MenuPrompt, Config.key)
-    PromptSetText(MenuPrompt, CreateVarString(10, 'LITERAL_STRING', _U('portPrompt')))
-    PromptSetVisible(MenuPrompt, true)
-    PromptSetStandardMode(MenuPrompt, true)
-    PromptSetGroup(MenuPrompt, PromptGroup)
-    PromptRegisterEnd(MenuPrompt)
-end
-
-function ManageBlip(shop, closed)
-    local shopCfg = Locations[shop]
-
-    if closed and not shopCfg.blip.showClosed then
-        if Locations[shop].Blip then
-            RemoveBlip(Locations[shop].Blip)
-            Locations[shop].Blip = nil
-        end
-        return
-    end
-
-    if not Locations[shop].Blip then
-        shopCfg.Blip = Citizen.InvokeNative(0x554d9d53f696d002, 1664425300, shopCfg.npc.coords) -- BlipAddForCoords
-        SetBlipSprite(shopCfg.Blip, shopCfg.blip.sprite, true)
-        Citizen.InvokeNative(0x9CB1A1623062F402, shopCfg.Blip, shopCfg.blip.name) -- SetBlipNameFromPlayerString
-    end
-
-    local color = shopCfg.blip.color.open
-    if shopCfg.shop.jobsEnabled then color = shopCfg.blip.color.job end
-    if closed then color = shopCfg.blip.color.closed end
-    Citizen.InvokeNative(0x662D364ABF16DE2F, Locations[shop].Blip, joaat(Config.BlipColors[color])) -- BlipAddModifier
-end
-
-function AddNPC(shop)
-    local shopCfg = Locations[shop]
-    if not shopCfg.NPC then
-        local modelName = shopCfg.npc.model
-        local model = joaat(modelName)
-        LoadModel(model, modelName)
-        shopCfg.NPC = CreatePed(model, shopCfg.npc.coords, shopCfg.npc.heading, false, false, false, false, false)
-        Citizen.InvokeNative(0x283978A15512B2FE, shopCfg.NPC, true) -- SetRandomOutfitVariation
-        SetEntityCanBeDamaged(shopCfg.NPC, false)
-        SetEntityInvincible(shopCfg.NPC, true)
-        Wait(500)
-        FreezeEntityPosition(shopCfg.NPC, true)
-        SetBlockingOfNonTemporaryEvents(shopCfg.NPC, true)
-    end
-end
-
-function RemoveNPC(shop)
-    local shopCfg = Locations[shop]
-    if shopCfg.NPC then
-        DeleteEntity(shopCfg.NPC)
-        shopCfg.NPC = nil
-    end
-end
-
-function LoadModel(model, modelName)
-    if not IsModelValid(model) then
-        return print('Invalid model:', modelName)
-    end
-    RequestModel(model, false)
-    while not HasModelLoaded(model) do
-        Wait(10)
-    end
 end
 
 AddEventHandler('onResourceStop', function(resourceName)
